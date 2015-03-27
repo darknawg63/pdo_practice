0aad2b11224c7bf0134a1f81fe5d31bd6076377b
darknawg63 <darknawg63@gmail.com>
2015-03-27 21:02:32 +0100

    PHP Data Objects (PDO): Fetching into a class (Part 4/8)

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+// Create the connection
 try {
     $conn = new PDO('mysql:host=127.0.0.1;dbname=employees', 'root', 'password');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -7,8 +7,19 @@ try {
     die($e->getMessage());
 }
 
-$query = $conn->query('SELECT * FROM employees LIMIT 6');
+class GuestbookEntry {
+    public $emp_no, $first_name, $last_name, $gender, $entry;
 
+    public function __construct() {
+        $this->entry = " Hi, my name is {$this->first_name}.";
+    }
+}
+
+// Create the query on the connection
+$query = $conn->query('SELECT * FROM employees LIMIT 6');
+// Set the fetch mode "FETCH_NUM, FETCH_BOTH, FETCH_ASSOC, FETCH_CLASS"
+$query->setFetchMode(PDO::FETCH_CLASS, 'GuestbookEntry');
+// Fetch the results from our query.
 while($r = $query->fetch()) {
-    echo $r['first_name'], '<br>';
+    echo $r->entry, '<br>';
 }
